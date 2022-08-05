5c052ab647708958006861a1d7d040ab9ff791c2
robester0403 <robkso@gmail.com>
2022-08-05 10:02:22 -0400

    Restructured Route Pages to make header and nested pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import FrontPage from "./pages/FrontPage";
-import HomeView from "./pages/HomeView";
+import MainView from "./pages/MainView";
 import "./styles/global.scss";
+import Home from "./views/Home";
 
 document.title = "Terra";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<FrontPage />} />
-      <Route path="/homepage" element={<HomeView />} />
+      <Route path="/front" element={<FrontPage />} />
+      <Route path="/" element={<MainView />}>
+        <Route path="home" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
