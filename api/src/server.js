ac32155674b98231d65601360f3fec1ee4b6cda5
Roy Anger <roy@royanger.com>
2022-08-17 22:21:55 -0400

    Installed prisma, setup basic models, confirmed connection to DB for reads and writes was working

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
-// dotenv.config()
+import dotenv from 'dotenv'
+dotenv.config()
+// import { PrismaClient } from '@prisma/client'
 
 // load vars from ENV variables
 const SECRET = process.env.SECRET
@@ -12,17 +14,43 @@ const app = express()
 app.use(express.json())
 
 app.use(
-  cors({
-    origin: process.env.APP_URL,
-    credentials: true,
-  })
+   cors({
+      origin: process.env.APP_URL,
+      credentials: true,
+   })
 )
 
 app.get('/hello-there', async (req, res) => {
-  res.json({ whom: 'Obi-Wan Kenobi' })
+   res.json({ whom: 'Obi-Wan Kenobi' })
 })
+
+// const prisma = new PrismaClient()
+// async function main() {
+//    // ... you will write your Prisma Client queries here
+
+//    await prisma.user.create({
+//       data: {
+//          email: 'royanger@gmail.com',
+//          provider: 'test',
+//          providerId: 'test2',
+//       },
+//    })
+
+//    const allUsers = await prisma.user.findMany()
+//    console.log(allUsers)
+// }
+
+// main()
+//    .then(async () => {
+//       await prisma.$disconnect()
+//    })
+//    .catch(async e => {
+//       console.error(e)
+//       await prisma.$disconnect()
+//       process.exit(1)
+//    })
 
 // start the server
 app.listen(PORT, URL, () => {
-  console.log(`EXPRESS: started on http://${URL}:${PORT}`)
+   console.log(`EXPRESS: started on http://${URL}:${PORT}`)
 })
